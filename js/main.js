d36f8ecfda31315ee938a946a77aac223ac8c50a
AndreLucyo2 <andre.lucyo2@gmail.com>
2023-07-30 18:11:24 -0300

    final commit

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ form.addEventListener('submit', (evento) => {
     const nome = evento.target.elements['nome'];
     const quantidade = evento.target.elements['quantidade'];
 
-    //Faz busca pelo nome: Percorre o array de elementos se encontra retorna o elemento
+    //Faz busca pelo nome: Percorre o array de elementos se encontrar retorna o elemento
     const itemExist = itens.find(element => element.nome === nome.value)
 
     //Cria o objeto js:
@@ -36,11 +36,12 @@ form.addEventListener('submit', (evento) => {
         atualizarElementoHTML(itemAtual);
 
         //Atualiza item do array: sobrescreve totalmente o item na posição
-        itens[itemExist.id] = itemAtual;
+        itens[itens.findIndex(element => element.id === itemExist.id)] = itemAtual;
 
     } else {
         //Se elemento não é encontrado cria o elemto novo com o id do ultimo elemento
-        itemAtual.id = itens.lengt;
+        itemAtual.id = itens[itens.length - 1] ? (itens[itens.length - 1]).id + 1 : 0; //verifica se o array esta vazio, caso nao estiver, obtem o ultimo id e somar mais um
+
         criarElementoHTML(itemAtual);
 
         //add novo item no array
@@ -49,7 +50,7 @@ form.addEventListener('submit', (evento) => {
     }
 
     //salva na memoria do navegador: array atualizado
-    addItensLocalStorage(itens);
+    setItensLocalStorage(itens);
 
     //Limpa o form
     limparForm({ nome: nome, quantidade: quantidade });
@@ -70,26 +71,56 @@ function criarElementoHTML(item) {
 
     //<strong>7</strong>
     const numeroItem = document.createElement('strong');
-    numeroItem.dataset.id = item.id;//cria um atributo nomeado
     numeroItem.innerHTML = item.quantidade;
+    numeroItem.dataset.id = item.id;//cria um atributo nomeado
 
     //<li class="item"><strong>7</strong>
     novoItem.appendChild(numeroItem);
+
     novoItem.innerHTML += item.nome;
+
+    //adiciona o elemento botao de deletar: informa o id do item a ser removido
+    novoItem.appendChild(criarBotaoDeletarById(item.id));
 
     //adiciona o elemento como filho da lista:
     lista.appendChild(novoItem);
 }
 
 function atualizarElementoHTML(item) {
-    //Vai atualizar para a quantida de atual do item: 
+    //Vai atualizar para a quantida de atual do item: já atualiza o dataSet
     document.querySelector("[data-id='" + item.id + "']").innerHTML = item.quantidade;
 }
 
-//Salva todo o arrau no local Storage : maximo até 5MB
-function addItensLocalStorage(arrItens) {
+//Salva todo o array no local Storage : maximo até 5MB
+function setItensLocalStorage(arrItens) {
     //Sava o array convertendo o JOSON em string pois, local Storage só permite guardar string
     localStorage.setItem(localStName, JSON.stringify(arrItens));
+}
+
+function criarBotaoDeletarById(id) {
+    const elementoBotao = document.createElement('button');
+    elementoBotao.innerText = "X";
+
+    //criar o eventlistner: precisa passar uma funsão para poder usar as propriedades do proprio elemento
+    elementoBotao.addEventListener('click', function () {
+        //remove o elemento html da pagina
+        deletaElemento(this.parentNode, id);
+    });
+
+    return elementoBotao;
+}
+
+function deletaElemento(tagHtml, id) {
+    tagHtml.remove();
+
+    //remover o item do array
+    console.log(id);
+
+    //romove o elemento pelo id:    
+    itens.splice(itens.findIndex(element => element.id === id), 1);
+
+    //atualizar o localstorage
+    setItensLocalStorage(itens);
 }
 
 
