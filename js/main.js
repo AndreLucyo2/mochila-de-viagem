aa0f2dd525ace45aea4e057023d0c436fb14f7c4
AndreLucyo2 <andre.lucyo2@gmail.com>
2023-07-30 09:53:28 -0300

    iniciando metodo para recuperar os dados do form

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log(document.getElementById("novoItem"));
+//console.log(document.getElementById("novoItem"));
+const form = document.getElementById("novoItem");
+
+form.addEventListener('submit', (evento) => {
+    //interrompe o comportamento padrão:
+    evento.preventDefault();
+
+    //obtem o objeto do evento:
+    console.log(evento);
+
+    //obtendo o array de elementos e seus valores pela posição: 
+    console.log(evento.target[0].value);
+    console.log(evento.target[1].value);
+
+    //obtendo o array de elementos e seus nomes : 
+    console.log(evento.target.elements['nome'].value);
+    console.log(evento.target.elements['quantidade'].value);
+
+})
